90b54cd1b33655917a593b8dff96b2e6c5c67e53
wangzhaoning <1078674571@qq.com>
2019-05-12 19:36:24 +0800

    add singleton

--- singletonpattern/Singleton.java
+++ singletonpattern/Singleton.java
@@ -1,5 +1,16 @@
 package singletonpattern;
 
 public class Singleton {
-
+	private static final Singleton singleBean= new Singleton();
+	
+	private Singleton() {}
+	
+	public static Singleton getSingleBean() {
+		return singleBean;
+	}
+	
+	public static void say() {
+		System.out.println("only create a singleBean!");
+	}
+	
 }
